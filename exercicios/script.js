e39339b48566b5946fc2e30c25e602d7c6c433dc
Caroline Vitoria Moreira dos Santos <1116802@senacminas.edu.br>
2026-09-01 16:26:43 -0300

    primeiro commit

---
+++ exercicios/script.js
@@ -0,0 +1,14 @@
+/* Atividade 4*/
+function verificarPrioridade() {
+    const CampoPrioridade = Text(document.getElementBlyd("Resultado").value);
+
+    let Resultado;
+    switch (CampoPrioridade) {
+        case "urgente": 
+        Resultado = "Atencao redobrada!";
+        break;
+        case "alta":
+        Resultado = ""
+
+    } 
+}
